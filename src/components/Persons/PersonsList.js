d8bbf3e5dd2690ddc8145e3214fd88245aa1fc42
Ulgamon <ilicjustin@gmail.com>
2023-04-14 18:16:36 +0200

    finished assignment

--- src/components/Persons/PersonsList.js
+++ src/components/Persons/PersonsList.js
@@ -3,16 +3,18 @@ import Person from "./Person"
 
 const PersonsList = props => {
     return (
-        <section className={`w-10/12 lg:max-w-screen-md mx-auto bg-gray-800 p-4 rounded-lg shadow-2xl`}>
+        <ul className={`w-10/12 lg:max-w-screen-md mx-auto bg-gray-800 p-4 rounded-lg shadow-2xl`}>
             {props.items.map(person => 
-                <Person 
-                    key={person.id}
-                    id={person.id}
-                    name={person.name}
-                    age={person.age}
-                />
+                <li key={person.id}>
+                    <Person 
+                        key={person.id}
+                        id={person.id}
+                        name={person.name}
+                        age={person.age}
+                    />
+                </li>
             )}
-        </section>
+        </ul>
     )
 }
 
